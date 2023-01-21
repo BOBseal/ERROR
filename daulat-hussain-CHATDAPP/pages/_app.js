4a034d3c081df63b65f00f218c71c24324391543
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-01-21 21:17:23 +0545

    gasless relay

--- daulat-hussain-CHATDAPP/pages/_app.js
+++ daulat-hussain-CHATDAPP/pages/_app.js
@@ -8,7 +8,12 @@ import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react';
 
 const App =({ Component, pageProps })=> {
   return (
-    <div><ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+    <div><ThirdwebProvider desiredChainId={ChainId.Mumbai}
+    sdkOptions={{gasless : { openzeppelin: { relayerUrl :
+      'https://api.defender.openzeppelin.com/autotasks/a6b4a873-d6dd-4428-97d3-8439a611ca08/runs/webhook/106c6781-828e-4cdb-b52a-a701e67bc1b3/P65B1JiFmaawTzrPQteThi'
+            
+      ,},},}}
+    >
     <ChatAppProvider>
       <NavBar/>
       <Component {...pageProps} />
