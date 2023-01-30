e72f172f3c3e8384c281aa2934972cccdd84458c
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-01-30 21:56:37 +0545

    ui Chat Window Update

--- daulat-hussain-CHATDAPP/Components/Friend/ChatWindow/Chat.jsx
+++ daulat-hussain-CHATDAPP/Components/Friend/ChatWindow/Chat.jsx
@@ -25,9 +25,9 @@ const Chat = ({seND,  friendMsg,
                     <path d="M18 18.7083C17.4832 16.375 15.5357 15 12.0001 15C8.46459 15 6.51676 16.375 6 18.7083M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21ZM12 12C13.3333 12 14 11.2857 14 9.5C14 7.71429 13.3333 7 12 7C10.6667 7 10 7.71429 10 9.5C10 11.2857 10.6667 12 12 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 <div className={S.ChatUserInfoBox}>
-                    <h4>{currentUserName}</h4>
+                    <h4>User: {currentUserName}</h4>
                     <p className={S.HDmobile}>
-                        {currentUserAddress}
+                       Address: {currentUserAddress.slice(0,3)}...{currentUserAddress.slice(38)}
                     </p>
                 </div>   
             </div>
@@ -35,10 +35,9 @@ const Chat = ({seND,  friendMsg,
         ): ("") }
 
         <div className={S.ChatBox}>
-            <div className={S.ChatBox_BOX}>
                 <div className={S.ChatBoxLeft}>
                     {friendMsg.map((el,i)=>(
-                        <div>
+                        <div className={S.Boxx}>
                             { el.sender == chatData.address ?(
                                 <div className={S.ChatBoxLeftTitle}>
                                     <span>
@@ -52,25 +51,24 @@ const Chat = ({seND,  friendMsg,
                                         <small>Time:{convertTime(el.timestamp)}</small>
                                     </span>
                             )}
-                            <p key={i+1}>
+                            <p key={i+1} className={S.msg}>
                               message:  {el.msg}
                             </p>
                         </div>
                     ))}
                 </div>
-            </div>
 
             {currentUserName && currentUserAddress ? (
             <>
                 <div className={S.ChatBoxSnd}>
                     <div className={S.ChatBoxSndImg}>
-                        <input type='text' placeholder='Type at least One Character' onChange={(e)=>setmessage(e.target.value)}/>
+                        <input type='text' placeholder='Type at least One Character' onChange={(e)=>setmessage(e.target.value)} className={S.INPUT}/>
                         {loading == true?(<Loader/>):(
-                        <div>
-                          { message? <button onClick={()=> seND({ address:currentUserAddress,msg: message})}><FaPaperPlane/></button>:<button>disabled</button>}
+                        <div className={S.SSSS}>
+                          { message? <button className={S.SSSS} onClick={()=> seND({ address:currentUserAddress,msg: message})}><FaPaperPlane/> SEND</button>:<button className={S.SSSS} >disabled</button>}
                         </div>
                         )}
-                        <button><FaPaperclip/></button>
+                        
                     </div>
                     
                 </div>
