3ac7d3568e62f047fd5d6e19533eb5d4dfdd696d
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-02-01 21:34:31 +0545

    Update about.js

--- daulat-hussain-CHATDAPP/pages/about.js
+++ daulat-hussain-CHATDAPP/pages/about.js
@@ -6,7 +6,7 @@ const about = () => {
         <h1>PERSONAL PROJECT BY BISHAL USING WEB3</h1>
         <p>HAS MANY BUGS AND IS IN ALPHA STAGE OF DEVELOPMENT</p>
         <p>CONTACT ME ON INSTAGRAM : @b.k.bh</p>
-        <h2>Please use Fantom Network for the app to work</h2>
+        <h2>Please use Polygon Mumbai Test Network for the app to work</h2>
     </div>
   )
 }
